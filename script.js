110b766bffa6255bdfa78d4537890461df553a2b
amitphalwadiya09 <151755174+amitphalwadiya09@users.noreply.github.com>
2024-03-28 03:35:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,24 @@
 const tbody = document.getElementById("output");
 
-tbody.innerHTML = `
-    <tr>
-        <td colspan="2">Loading...</td>
-    </tr>
-`;
-
 function createPromise(delay) {
     return new Promise((resolve, reject) => {
-
         setTimeout(() => {
             resolve(`${delay/1000}`);
         }, delay);
     });
 }
 
-const promises=[createPromise(2000, "Promise 1"),createPromise(1000, "Promise 2"),createPromise(2580, "Promise 3")]
-
-
+const promises = [
+    createPromise(2000),
+    createPromise(1000),
+    createPromise(2580)
+];
 
 Promise.all(promises)
     .then(results => {
-        tbody.innerHTML = ""; 
-        
-        results.forEach((time,index) => {
+        tbody.innerHTML = ""; // Clear the loading row 
+
+        results.forEach((time, index) => {
             const row = document.createElement('tr');
             row.innerHTML = `
                 <td>Promise ${index + 1}</td>
@@ -31,10 +26,10 @@ Promise.all(promises)
             `;
             tbody.appendChild(row);
         });
+
         let totalTime = 0;
-        for(let i=0;i<results.length;i++)
-        {
-            totalTime=totalTime+parseFloat(results[i]);
+        for (let i = 0; i < results.length; i++) {
+            totalTime = totalTime + parseFloat(results[i]);
         }
 
         const totalRow = document.createElement('tr');
